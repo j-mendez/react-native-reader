6c6ebc6f5b478077f3c44edebd769a7cc26a0a52
Serdar Senay <lupelius@users.noreply.github.com>
2019-06-14 01:50:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
 import {
-  ActivityIndicator,
-  StyleSheet,
-  View
-} from 'react-native';
-import { WebView } from 'react-native-webview';
-import Readability from './util/readability';
-import cleanHtmlTemplate from './util/cleanHtmlTemplate';
-import cleanHtmlCss from './util/cleanHtmlCss';
+  cleanHtml,
+  cleanHtmlCss,
+  cleanHtmlTemplate,
+  defaultHtmlCss
+} from "./util";
 
-const css = `
-  body {
-    color: #2a2a2a;
-    font-family: sans-serif, normal, San Francisco;
-  }
-  img, figure {
-    display: none;
-  }
-  h1 {
-    border-bottom-width: 1px;
-    border-color: #ccc;
-    padding-bottom: 3px;
-    border-bottom-style:solid;
-    font-size: 1.6em;
-    font-weight: bold;
-    margin-bottom: 5px;
-    letter-spacing: .05em;
-  }
-  p {
-    letter-spacing: .03em;
-  }
-`;
-
-export default class ReadabilityWebView extends Component {
+export default class ReadabilityWebView extends PureComponent {
   static defaultProps = {
-    url: '',
-    htmlCss: css,
-    title: '',
+    url: "",
+    htmlCss: defaultHtmlCss,
+    title: "",
     onError: null,
     readerMode: true,
     renderLoader: null
@@ -44,82 +20,106 @@ export default class ReadabilityWebView extends Component {
 
   state = {
     cleanHtmlSource: undefined,
-    readabilityArticle: null,
+    readabilityArticle: null
   };
 
-  refreshCSS = () => {
-    this.state.readabilityArticle &&
-      this.setState((state, props) => ({
-        cleanHtmlSource: cleanHtmlTemplate(this.props.css, props.title || state.readabilityArticle.title, state.readabilityArticle.content)
-      }));
-  }
-
-  async componentDidMount(){
+  async componentDidMount() {
     const { url, htmlCss, title, readerMode } = this.props;
 
-    if(!readerMode) {
-      this.setState({cleanHtmlSource: false});
+    if (!readerMode) {
+      this.toggleReaderMode();
       return;
     }
     try {
       const response = await fetch(url);
       const html = await response.text();
-      let cleanHtml = '';
 
-      const readabilityArticle = await Readability.cleanHtml(html, url);
+      const readabilityArticle = await cleanHtml(html, url);
 
-      if(!readabilityArticle) {
-        cleanHtml = false;
-      } else if (htmlCss) {
-        cleanHtml = cleanHtmlTemplate(htmlCss, title || readabilityArticle.title, readabilityArticle.content);
-      } else {
-        cleanHtml = cleanHtmlTemplate(cleanHtmlCss, title || readabilityArticle.title, readabilityArticle.content);
-      }
-      this.props.returnHtmlText(`${title || readabilityArticle.title}. ${readabilityArticle.content}`);
       this.setState({
-        cleanHtmlSource: cleanHtml,
+        cleanHtmlSource: !readabilityArticle
+          ? false
+          : cleanHtmlTemplate(
+              htmlCss ? htmlCss : cleanHtmlCss,
+              title || readabilityArticle.title,
+              readabilityArticle.content
+            ),
         readabilityArticle
       });
-
     } catch (err) {
-      if(this.props.onError) {
+      if (this.props.onError) {
         this.props.onError(err);
       }
     }
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.css !== prevProps.css) {
-      this.refreshCSS();
+    if (this.props.htmlCss !== prevProps.htmlCss) {
+      this.refreshHtml();
+    } else if (this.props.readerMode !== prevProps.readerMode) {
+      this.toggleReaderMode(this.props.readerMode);
     }
-    
   }
 
+  refreshHtml = () => {
+    if (this.state.readabilityArticle) {
+      this.setState({
+        cleanHtmlSource: cleanHtmlTemplate(
+          this.props.htmlCss,
+          props.title || this.state.readabilityArticle.title,
+          this.state.readabilityArticle.content
+        )
+      });
+    }
+  };
+
+  toggleReaderMode = (cleanHtmlSource = false) => {
+    if (!cleanHtmlSource) {
+      this.setState({ cleanHtmlSource });
+    } else {
+      this.refreshHtml();
+    }
+  };
+
   render() {
+    const {
+      containerStyle,
+      loadingContainerStyle,
+      indicatorProps
+    } = this.props;
+
     return (
-       <View style={styles.flex}>
-        { this.state.cleanHtmlSource === undefined ?
-        (this.props.renderLoader ?
-          this.props.renderLoader
-            :
+      <View style={[styles.flex, containerStyle]}>
+        {this.state.cleanHtmlSource === undefined ? (
+          this.props.renderLoader ? (
+            this.props.renderLoader
+          ) : (
             <View
-            style={[styles.flex, styles.loadingContainer]}
-          >
-            <ActivityIndicator />
-          </View>
+              style={[
+                styles.flex,
+                styles.loadingContainer,
+                loadingContainerStyle
+              ]}
+            >
+              <ActivityIndicator {...indicatorProps} />
+            </View>
           )
-         : (this.state.cleanHtmlSource === false ?
-           <WebView
-             style={ styles.flex }
-             source={{uri: this.props.url}}
-             />
-           :
-           <WebView
-             style={ styles.flex }
-             source={ {html: this.state.cleanHtmlSource, baseUrl: this.props.url}}
-             />
-         )
-       }
+        ) : this.state.cleanHtmlSource === false ? (
+          <WebView
+            style={styles.flex}
+            source={{ uri: this.props.url }}
+            {...this.props}
+          />
+        ) : (
+          <WebView
+            style={styles.flex}
+            source={{
+              html: this.state.cleanHtmlSource,
+              baseUrl: this.props.url
+            }}
+            {...this.props}
+          />
+        )}
       </View>
     );
   }
@@ -127,10 +127,10 @@ export default class ReadabilityWebView extends Component {
 
 const styles = StyleSheet.create({
   flex: {
-    flex: 1,
+    flex: 1
   },
   loadingContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
